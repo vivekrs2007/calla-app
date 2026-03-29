537e3b73f4e84aeaa34c1149195172d32e1d715b
vivekrs2007 <your@email.com>
2026-03-29 00:01:21 -0400

    Add publicDir to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  build: {
+    target: 'es2020',
+  },
+  define: {
+    'self.FIREBASE_MESSAGING_SW': false,
+  },
 })
